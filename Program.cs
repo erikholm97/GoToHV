af73a5eb169deada88c3cd75de66fcb9513249da
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2020-09-19 18:25:47 +0200

    Update Program.cs
    Possability to login to hv.se

--- Program.cs
+++ Program.cs
@@ -10,31 +10,47 @@ namespace GoToHV
         static void Main(string[] args)
         {
             Console.WriteLine("Hello!");
-            //Console.WriteLine("Enter your credentials: Username and password");
-            //string username = Console.ReadLine();
-            //string password = Console.ReadLine();
+            Console.WriteLine("Enter your credentials: Username and password");
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
 
-            //Users user = new Users();
+            Users user = new Users();
 
-            //user.UserName = username;
-            //user.Password = password;
+            user.UserName = username;
+            user.Password = password;
 
             var driver = new ChromeDriver();
 
             driver.Url = "https://www.hv.se";
 
-            IWebElement element = driver.FindElement(By.ClassName("cf-container__close")); // locate the button, can be done with any other selector
-            Thread.Sleep(1000);
+            IWebElement element = driver.FindElement(By.XPath("/ html / body / div[1] / header / div[1] / div / div / nav / ul / li[1] / a")); // locate the button, can be done with any other selector
+
             element.Click();
-            Console.ReadLine();
 
-            //laddar, finns ett problem
-            IWebElement element2 = driver.FindElement(By.LinkText("Student")); // locate the button, can be done with any other selector
             Thread.Sleep(1000);
-            element2.Click();
-            Console.ReadLine();
+            element = driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div[4]/div/div[1]/div/div/div/div/ul/li[1]/a"));
 
+            element.Click();
 
+            Thread.Sleep(1000);
+            element = driver.FindElement(By.XPath("/ html / body / div[1] / div[2] / div[10] / div / div / article / a"));
+
+            element.Click();
+
+           
+            Thread.Sleep(1000);
+            element = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div/main/div/div/div/form/div[2]/div[1]/input"));
+            element.SendKeys(username);
+
+            Thread.Sleep(1000);
+            element = driver.FindElement(By.XPath("/ html / body / div[2] / div[2] / div / main / div / div / div / form / div[2] / div[2] / input"));
+
+            element.Click();
+            element.SendKeys(password);
+
+            element = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div/main/div/div/div/form/div[2]/div[4]/span"));
+
+            element.Click();
         }
     }
 }
