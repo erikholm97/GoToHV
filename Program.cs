67a00b72cead72f321e795c5b10d81197f581810
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2020-09-19 10:52:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoToHV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
